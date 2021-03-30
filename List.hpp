5ac88e0ff32408524a198983a9c4552ff41b1a5f
CiberAdventureCat <kbatwoma@student.21-school.ru>
2021-03-30 15:32:45 +0300

    start

---
+++ List.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   List.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbatwoma <kbatwoma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/30 15:31:59 by kbatwoma          #+#    #+#             */
+/*   Updated: 2021/03/30 15:32:02 by kbatwoma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIST_HPP
+# define LIST_HPP
+
+# include <iostream>
+
+#include <list>
+std::list<int> list_;
+
+
+namespace ft
+{
+    template <typename Content_type>
+    class List
+    {
+        public:
+            List();
+            List(List const &);
+            List &operator=(List const &);
+            ~List();
+            
+
+        private:
+
+    };
+}
+
+#endif
